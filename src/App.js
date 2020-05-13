5e7874e72f972470210c4bcc17d7c4b47fd72d53
pavithra <pavithragowda.m@terralogic.com>
2020-05-13 12:39:53 +0530

    created folder structure

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import login from './Components/Login/login';
+import dashboard from './Components/Dashboard/dashboard';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={login} />
+        <Route path="/dashboard" component={dashboard} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './login.scss';
+// import logo from '../../assets/images/'
+export class login extends Component {
+  render() {
+    return (
+      <div className="login-css">
+        {/* <img src={logo} alt="logo" /> */}
+      </div>
+    )
+  }
+}
+
+export default login
